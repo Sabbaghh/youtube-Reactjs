3da0a98e5404c99af530a14545eba928649d9bfc
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-09-16 05:51:29 +0300

    Updates

---
+++ precache-manifest.2f8244d77959013fb4c292e8f531bbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ff0044dcf4f1b86a04329fd8f70ce3",
+    "url": "/youtube-Reactjs/index.html"
+  },
+  {
+    "revision": "cce03d40fa0329e3324d",
+    "url": "/youtube-Reactjs/static/css/main.32e44825.chunk.css"
+  },
+  {
+    "revision": "5b4b1cb0104b6f38ad6d",
+    "url": "/youtube-Reactjs/static/js/2.ca109c27.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-Reactjs/static/js/2.ca109c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce03d40fa0329e3324d",
+    "url": "/youtube-Reactjs/static/js/main.88335832.chunk.js"
+  },
+  {
+    "revision": "cd63efc65bac2a192d4a",
+    "url": "/youtube-Reactjs/static/js/runtime-main.c61ffc71.js"
+  }
+]);
